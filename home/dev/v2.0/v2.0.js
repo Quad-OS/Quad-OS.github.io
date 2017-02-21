3f64e58349e60b4660edb7fb670b2b958c9a26ed
Quad-OS <Quad-OS@users.noreply.github.com>
2017-02-21 10:22:41 -0500

    Update v2.0.js

--- home/dev/v2.0/v2.0.js
+++ home/dev/v2.0/v2.0.js
@@ -1,1 +1,49 @@
+function notifyBox() {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    var notification = new Notification("Box does support saving files in its current version, but only up to 5 megabytes.");
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function(permission) {
+      if (permission === "granted") {
+        var notification = new Notification("Box does support saving files in its current version, but only up to 5 megabytes.");
+      }
+    });
+  }
+}
+Notification.requestPermission().then(function(result) {
+  console.log(result);
+});
 
+function spawnNotification(theBody, theIcon, theTitle) {
+  var options = {
+    body: theBody,
+    icon: theIcon
+  }
+  var n = new Notification(theTitle, options);
+}
+
+function notifyScribble() {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    var notification = new Notification("Scribble does not support saving files in its current version.");
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function(permission) {
+      if (permission === "granted") {
+        var notification = new Notification("Scribble does not support saving files in its current version.");
+      }
+    });
+  }
+}
+Notification.requestPermission().then(function(result) {
+  console.log(result);
+});
+
+function spawnNotification(theBody, theIcon, theTitle) {
+  var options = {
+    body: theBody,
+    icon: theIcon
+  }
+  var n = new Notification(theTitle, options);
+}
